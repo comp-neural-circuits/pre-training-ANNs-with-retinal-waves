2026316573ca598375879e87393abf750def2b21
Lilly May <93096564+Lilly-May@users.noreply.github.com>
2024-09-10 13:23:54 +0200

    Update blender_maze_generation.py

--- datasets/blender_maze_generation.py
+++ datasets/blender_maze_generation.py
@@ -42,12 +42,12 @@ cam.rotation_euler[2] = -90 * (math.pi / 180.0)
 
 bpy.ops.curve.primitive_nurbs_path_add(radius=1, enter_editmode=False, align='WORLD', location=((total_nr_images/2), 0, 0.5), scale=(1, 1, 1))
 
-bpy.context.space_data.context = 'WORLD'
+#bpy.context.space_data.context = 'WORLD'
 bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (0.522517, 0.522517, 0.522517, 1)
 
 
-#TODO in blender:
-# 1. set render settings: y=128, x=64
-# 2. create path (shift + A, path, set size and position); then tab (go into object mode); click on first node; shift S; Cursor to selected; Select camere; shift S; Selection to cursor; select camera and path; cmd + P; follow path
-# 3. adjust speed of camera: click on path, then object data properties on the right, then path animation and change number of frames
+#TODO manually in blender:
+# 1. Under "Output Properties" on the right: set render settings: y=64, x=128, set frame range (e.g. 1 to 6000), set output directory
+# 2. create path (shift + A, path (maybe under "curve"), set size and position); then tab (go into object mode); click on first node; shift S; Cursor to selected; (if necessary, go into object mode by clicking tab again) Select camera; shift S; Selection to cursor; select camera and path; cmd + P; follow path
+# 3. adjust speed of camera: click on path, then object data properties on the right, then path animation and change number of frames (e.g. 6000)
 # 4. set light as child of camera (on the right, under "Object Constraint Properties")
